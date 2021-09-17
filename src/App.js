71c95994f7c5a20c9ceca37ae18a28f7b0cc8c59
ArcOmotola <ogunsola40@gmail.com>
2021-09-17 18:43:58 +0100

    completed searchResult page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Home from './Home';
 import Header from './Header';
 import Footer from './Footer';
+import SearchPage from './SearchPage.js';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function App() {
     return (
         <div className="app">
-            {/* <h1>Airbnb clone</h1> */}
-            <Header />
-            <Home />
-            <Footer />
+            <Router>
+                <Header />
+
+                    <Switch>
+                        <Route path="/search">
+                            <SearchPage />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+
+                <Footer />
+            </Router>
         </div>
 
         
